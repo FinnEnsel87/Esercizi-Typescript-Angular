54735fa82eac5b9a8b991e3b26205359018c6a2b
FinnEnsel87 <fgsantaniello@gmail.com>
2021-11-30 16:59:32 +0100

    fix: Angular-es.1 fixed in local

---
+++ src/app/models/User.ts
@@ -0,0 +1,43 @@
+export enum Role {
+    STAFF = "staff",
+    STUDENT = "student",
+    MANAGER = "manager",
+    ADMIN = "admin",
+}
+
+export enum Gender {
+    MALE = "male",
+    FEMALE = "female",
+    OTHER = "other",
+}
+
+export interface Address {
+    city: string,
+    street: string,
+    postalCode: string 
+}
+
+export interface Location {
+    city: string,
+    street: string,
+    postalCode: string
+}
+export interface Company {
+    id: number,
+    name: string,
+    description: string,
+    location: Location
+}
+export interface User {
+    id: number,
+    name: string,
+    surname: string,
+    age: number,
+    dateOfBirth: string,
+    address: Address,
+    role: Role,
+    username: string,
+    profilePhotoUrl: string,
+    companies?: Company[],
+    gender: Gender
+}
